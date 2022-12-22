a24557e48c8558acc97818b87445a2b4c0564d08
Roshan <nkroshankumar@gmail.com>
2022-12-22 17:56:09 +0530

    useAxios hook created

---
+++ src/hooks/useAxios.tsx
@@ -0,0 +1,77 @@
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+
+//https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=inr&days=20
+
+//https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false
+
+interface CoinDetailsProps {
+    name: string
+    description: {
+        en: string
+    }
+    image: {
+        thumb: string
+        small: string
+        large: string
+    }
+    market_cap_rank: number
+    market_data: {
+        current_price: {
+            inr: number
+            usd: number
+        }
+    }
+}
+
+interface ChartDetailsProps {
+    x: number
+    y: number
+}
+
+const useAxios = (coin: string) => {
+    const [coinDetails, setCoinDetails] = useState({} as CoinDetailsProps)
+    const [chartDetails, setChartDetails] = useState({} as ChartDetailsProps[])
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+
+    const fetchData = async (coin: string) => {
+        try {
+            setIsLoading(true)
+            const coinDetails = await axios.get(
+                `https://api.coingecko.com/api/v3/coins/${coin}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`
+            )
+
+            setCoinDetails(coinDetails.data)
+
+            const chartDetails = await axios.get(
+                `https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=inr&days=7`
+            )
+
+            const chartCoords = chartDetails.data.prices.map(
+                (price: number[]) => ({ x: price[0], y: price[1].toFixed(2) })
+            )
+
+            setChartDetails(chartCoords)
+        } catch (e) {
+            setIsError(true)
+            console.log(e)
+        } finally {
+            setIsLoading(false)
+            setIsError(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData(coin)
+    }, [coin])
+
+    return {
+        isLoading,
+        isError,
+        coinDetails,
+        chartDetails,
+    }
+}
+
+export default useAxios
