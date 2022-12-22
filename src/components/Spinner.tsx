7d7e89870e5348ba5b3ff067eac40632776a1cf4
Roshan <nkroshankumar@gmail.com>
2022-12-22 17:56:42 +0530

    Spinner Comp created

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Spinner = () => {
+    return (
+        <div className="w-12 h-12 rounded-full border-4 border-black border-t-gray-500 animate-spin my-20 mx-auto"></div>
+    )
+}
+
+export default Spinner
